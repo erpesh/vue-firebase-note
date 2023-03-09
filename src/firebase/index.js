3644674cc021f082a22c2b3aaf57dc64ad3908c3
erpesh <89254828+erpesh@users.noreply.github.com>
2023-03-09 23:39:05 +0000

    .env file fixes

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import dotenv from 'dotenv';
+dotenv.config();
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
